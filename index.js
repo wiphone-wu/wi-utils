67b6ad958c885004e3cd8629144e24b6bcc2567c
wiphone <wiphone41@outlook.com>
2023-03-17 16:31:07 +0800

    导入方式优化 新增babel和commonjs

---
+++ index.js
@@ -0,0 +1,3 @@
+'use strict'
+
+module.exports = require('./dist/witis.cjs.js')
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,21 +1,20 @@
 import typeScript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import terser from "@rollup/plugin-terser";
 
 export default {
   input: "./index.ts", //入口文件
-  output: [{
+  output: {
     // dir: "dist", //打包后的存放文件
-    format: "cjs", //输出格式 amd es6 iife umd cjs
+    format: "cjs", //输出格式 amd es iife umd cjs
     name: "bundleName", //如果iife, umd需要指定一个全局变量
-    file: 'dist/witis.js' //自定义打包存储文件
-  },{
-    // dir: "dist", //打包后的存放文件
-    format: "es", //输出格式 amd es6 iife umd cjs
-    name: "bundleName", //如果iife, umd需要指定一个全局变量
-    file: 'dist/witis.ts' //自定义打包存储文件
-  }],
+    file: 'dist/witis.cjs.js' //自定义打包存储文件
+  },
   plugins: [
     typeScript(),
-    terser(), // 压缩代码工具
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    terser() // 压缩代码工具
   ],
 };
